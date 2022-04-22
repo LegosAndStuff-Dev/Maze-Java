5e1bc6e5244f5e24cebb40675f5a50eb77ecee85
LegosAndStuff <55319190+LegosAndStuff-Dev@users.noreply.github.com>
2022-04-22 10:18:41 -0500

    Add files via upload

---
+++ Maze_MasonNorberg.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Maze_MasonNorberg
+{
+	public static void main (String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		Maze maze = new Maze();
+		
+		maze.displayOutput();
+	}
+}
