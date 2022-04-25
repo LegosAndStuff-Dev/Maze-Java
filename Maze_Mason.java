817585dbb5220642cea2e8e0e4ba745f189ae50c
LegosAndStuff <55319190+LegosAndStuff-Dev@users.noreply.github.com>
2022-04-25 10:29:26 -0500

    Add files via upload

---
+++ Maze_Mason.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Maze_MasonNorberg
+{
+	public static void main (String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		Maze maze = new Maze();
+		
+		boolean gameRunning = true;
+		boolean vaildMove;
+		String move;
+		
+		maze.displayOutput();
+		
+		//maze.move("d");
+		
+		//maze.displayOutput();
+		
+		while (gameRunning) {
+			System.out.print("Enter move: ");
+			move = input.next();
+			
+			System.out.println(move);
+			//vaildMove = maze.move(move);
+			maze.move("d");
+			vaildMove = true;
+			
+			if (vaildMove == true) {
+				maze.displayOutput();
+			}
+			
+			else {
+				System.out.println("An error happened please try again");
+			}
+		}
+	}
+}
